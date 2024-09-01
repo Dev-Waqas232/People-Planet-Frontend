a3fe4afc105ff3644407adf952ad0ce86190b019
Waqas Munir <devwaqas232@gmail.com>
2024-09-01 23:36:22 +0500

    Completed User Profile Card Ui

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,4 +1,7 @@
 import { useEffect } from 'react';
+import { FaRegCalendarAlt } from 'react-icons/fa';
+import { CiMail } from 'react-icons/ci';
+
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { getUser } from '../features/user/userActions';
 
@@ -12,13 +15,39 @@ export default function UserProfile() {
   }, []);
 
   return (
-    <div>
-      <div>
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD4qmuiXoOrmp-skck7b7JjHA8Ry4TZyPHkw&s"
-          alt="cover photo"
-        />
-        {user?.firstName}
+    <div className="px-8 flex">
+      {/* User Profile */}
+      <div className="w-1/4 flex flex-col bg-white px-8 py-8 my-12 gap-6">
+        <div className="flex justify-center items-center">
+          <img
+            className="w-3/4 rounded-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa6YvRump6DC1zR3Bu5fz9358Gcgviuu5nag&s"
+            alt=""
+          />
+        </div>
+        <div>
+          <h2 className="text-3xl font-primary text-center">
+            {' '}
+            {user?.firstName} {user?.lastName}{' '}
+          </h2>
+          <p className="text-center">ADD BIO</p>
+          <div className="mt-4">
+            <p className="flex items-center gap-1">
+              <FaRegCalendarAlt />
+              <span>Born at {}</span>
+            </p>
+            <p className="flex items-center gap-1">
+              <CiMail />
+              <span>{user?.email}</span>
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center mt-4">
+            <button className="font-primary bg-primary w-full px-2 py-2 rounded-md text-white">
+              Update Profile
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
