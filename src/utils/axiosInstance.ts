1cfed29065cf978eeb2800637a3bf61c243b758c
Waqas Munir <devwaqas232@gmail.com>
2024-09-26 12:30:43 +0500

    Changed Url

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -5,7 +5,7 @@ import axios, {
 } from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://people-planet.onrender.com/api',
 });
 
 axiosInstance.interceptors.request.use(
